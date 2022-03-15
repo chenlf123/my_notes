31bc690f4178756ac49221b806e8f985795dc547
ChenLinfei <a750197579@qq.com>
2022-03-15 06:32:34 -0700

    update

---
+++ qemu/build.sh
@@ -0,0 +1,2 @@
+sudo apt install zlib1g-dev libglib2.0-0 libglib2.0-dev libtool libsdl1.2-dev autoconf ninja-build libpixman-1-0 libpixman-1-dev
+../configure --target-list=arm-softmmu --audio-drv-list=
